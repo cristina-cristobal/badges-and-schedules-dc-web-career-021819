ddd5437742330c032c4596fd49e4bae15ac684d7
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-02-06 17:19:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  badge_messages = []
+  array.each do |name|
+    badge_messages << badge_maker(name)
+  end
+  badge_messages
+end
